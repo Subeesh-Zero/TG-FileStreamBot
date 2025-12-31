a7969778c664a001712bc1d60e9a383dc1c498c6
Subeesh <it.database.in@gmail.com>
2026-01-01 01:16:52 +0530

    Refactor vars.py for improved configuration management
    Refactor environment variable handling and add new variables.

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,41 +1,41 @@
-
-# (c) adarsh-goel
-import os
-from os import getenv, environ
+# This file is a part of TG-FileStreamBot
+import sys
+from os import environ
 from dotenv import load_dotenv
-
-
 
 load_dotenv()
 
+
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '22338436'))
-    API_HASH = str(getenv('API_HASH', 'dfc5f483a41f358cca2299d01ebe2bee'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '8591451697:AAGxk_TGodPFLlhEbX-cbUm2_N4iQMZYovc'))
-    name = str(getenv('name', 'subeeshFbot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1003497661141'))
-    PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "8080221131").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'tel_zero'))
-    if 'DYNO' in environ:
-        ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
-    
-    else:
-        ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
-    if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
-    else:
-        URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://subeesh2702:X0bgwLEzQj5DhpHP@cluster0.5a5my.mongodb.net/?appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'MovieXBots'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    USERNAME: str = None
+    FIRST_NAME: str = None
+
+    API_ID = int(environ.get("API_ID", 0))
+    API_HASH = str(environ.get("API_HASH", ""))
+    BIN_CHANNEL = int(
+        environ.get("BIN_CHANNEL", None)
+    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+
+    ALLOWED_USERS = [int(x.strip()) for x in environ.get("ALLOWED_USERS", "").split(",") if x.strip()]
+    BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
+    BLOCKED_USERS = [int(x.strip()) for x in environ.get("BLOCKED_USERS", "").split(",") if x.strip()]
+    CACHE_SIZE: int = int(environ.get("CACHE_SIZE", 128))
+    CHUNK_SIZE: int = int(environ.get("CHUNK_SIZE", 1024 * 1024)) #bytes
+    CONNECTION_LIMIT = int(environ.get("CONNECTION_LIMIT", 20))
+    DEBUG: bool = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
+    FQDN = str(environ.get("FQDN", BIND_ADDRESS))
+    HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
+    HASH_LENGTH = int(environ.get("HASH_LENGTH", 6))
+    if not 5 < HASH_LENGTH < 64:
+        sys.exit("Hash length should be greater than 5 and less than 64")
+    KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "0").lower()) in  ("1", "true", "t", "yes", "y")
+    NO_PORT = str(environ.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
+    NO_UPDATE = str(environ.get("NO_UPDATE", "0").lower()) in ("1", "true", "t", "yes", "y")
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "600"))  # 10 minutes
+    PORT = int(environ.get("PORT", 8080))
+    REQUEST_LIMIT = int(environ.get("REQUEST_LIMIT", 5))
+    SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
+    TRUST_HEADERS: bool = str(environ.get("TRUST_HEADERS", "1").lower()) in ("1", "true", "t", "yes", "y")
+    URL = f"http{"s" if HAS_SSL else ""}://{FQDN}{"" if NO_PORT else ":" + str(PORT)}/"
